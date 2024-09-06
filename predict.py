8c05eaaf280fe7e891cd47eba5ad7cefc00e1605
Joshua Stahl <neobaud@gmail.com>
2024-09-06 13:16:40 -0500

    Fixed the problem with future bleed.  Results should be accurate at least if not good.

--- predict.py
+++ predict.py
@@ -12,6 +12,7 @@ from sklearn.naive_bayes import GaussianNB
 from catboost import CatBoostClassifier
 from sklearn.neural_network import MLPClassifier
 from optparse import OptionParser
+from sklearn.decomposition import PCA
 
 # from xgboost import XGBClassifier
 
@@ -25,7 +26,7 @@ models = {
     'K-Nearest Neighbors': KNeighborsClassifier(),
     'Support Vector Machine': SVC(),
     'Decision Tree': DecisionTreeClassifier(random_state=SEED),
-    'Random Forest': RandomForestClassifier(n_estimators=100, random_state=SEED),
+    'Random Forest': RandomForestClassifier(n_estimators=500, random_state=SEED),
     'Gradient Boosting': GradientBoostingClassifier(),
     'AdaBoost': AdaBoostClassifier(),
     'Naive Bayes': GaussianNB(),
@@ -85,7 +86,7 @@ def load_from_pickle_cache(data_info):
         data = pickle.load(f)
     return data
 
-def fetch_data_with_cache(start_year, end_year, end_week=17):
+def fetch_data_with_cache(start_year, end_year, end_week, options):
     all_pbp_data = []
     game_cols =['game_id', 'home_team', 'away_team', 'total_home_score', 'total_away_score', 'week', 'game_date', 'season_type', 'year']
     off_filter = {'game_id2': 'first', 'team': 'first', 'home_away':'first', 'drive':'max', 'touchdown':'sum','field_goal_result':'sum', 'total_home_score': 'max', 'total_away_score': 'max',}
@@ -96,16 +97,20 @@ def fetch_data_with_cache(start_year, end_year, end_week=17):
     # try to load the data from file cache
     data = load_from_pickle_cache(data_info)
 
-    if data is not None:
+    if data is not None and not options.force_data_gather:
         return data, stats_cols, game_cols
 
     for season in range(start_year, end_year + 1):
+        print(f"Fetching data for {season}...")
         # Fetch play-by-play data
         pbp_data = get_cached_data(season, "pbp")
         if pbp_data is None:
+            print(f"Cache miss.. Fetching play-by-play data for {season}...")
             pbp_data = nfl.import_pbp_data([season])
             save_to_cache(pbp_data, season, "pbp")
         pbp_data['year'] = season
+        if season == end_year:
+            pbp_data = pbp_data[pbp_data['week'] <= end_week]
         all_pbp_data.append(pbp_data)
 
         # # Fetch drive data
@@ -163,18 +168,35 @@ def fetch_data_with_cache(start_year, end_year, end_week=17):
     #final_summary = final_summary.rename(columns={'game_id2': 'game_id'})
 
     # need to get the previous win because the current win is what we are trying to predict and so won't be available
-    final_summary['previous_win'] = final_summary.groupby('team')['result'].shift(1).dropna()
+    final_summary['previous_win'] = final_summary['result']#final_summary.groupby('team')['result'].shift(1).dropna()
 
-    results = final_summary[['game_id', 'team', 'result']]
+    # results = final_summary[['game_id', 'team', 'result']]
     final_summary = final_summary[stats_cols + game_cols + ['team', 'home_away', 'result']]
+
+    # This part shifts the game rows so that the result of the game is aligned with the stats of the previous game
+    # Loop through each team separately and shift the result column back one.
+    for team in final_summary['team'].unique():
+        team_data = final_summary[final_summary['team'] == team]
+        team_data['result'] = team_data['result'].shift(-1).dropna()
+        final_summary[final_summary['team'] == team] = team_data
+
 
     # save all data to pickle cache file
     save_to_pickle_cache(final_summary, data_info)
 
     return final_summary, stats_cols, game_cols
 
+def weighted_rolling_mean(series, window_size, weights):
+
+    return result
+
 
 def filter_stats(final_summary, stats):
+    filter_size = 20
+    weights = None
+    alpha = .2
+    season_discount = .9
+
     # apply a weighted average filter to final_summary 
     # to get the stats that we want
     # Loop through each team separately
@@ -182,13 +204,28 @@ def filter_stats(final_summary, stats):
         team_data = final_summary[final_summary['team'] == team]
         # run a filter on the data that weights the latest games more
         # and returns the stats that we want
-        team_data.loc[:, stats] = team_data[stats].rolling(window=10).mean()
+        filter_window = 30
+        # team_data.loc[:, stats] = team_data[stats].rolling(window=filter_window).mean()
+        # # chop off the filter window
+        # team_data = team_data[filter_window:]
+        years=team_data['year']
+        year_scaled = (years - years.iloc[0]+1)
+        pre_compute = np.exp(pd.Series(range(len(years))) * np.log(1-alpha)).iloc[::-1].reset_index(drop=True)
+        discount_weights = np.exp(year_scaled * np.log(season_discount)).iloc[::-1].reset_index(drop=True)
+
+        def __windowed_average__(x):
+            x=x.reset_index(drop=True)
+            window = pre_compute[:len(x)] * discount_weights[:len(x)]
+            return np.sum(window * x) / window.sum()
+        # team_data.loc[:, stats] = team_data[stats].apply(__windowed_average__)
+        # team_data.loc[:, stats] = team_data[stats].ewm(alpha=.2).mean()
+        team_data.loc[:, stats] = team_data[stats].expanding().apply(__windowed_average__)
         # take a windowed average of the last 10 games
         # put team_data back into final_summary
         final_summary[final_summary['team'] == team] = team_data
 
     # Display the first few rows of the DataFrame
-    return final_summary.dropna()
+    return final_summary
 
 
 
@@ -203,9 +240,11 @@ def print_correlation(final_summary, stat):
     plt.title('%s vs. win rate' % stat_label)
     plt.show()
 
-def find_correlation(pbp_data, stats,):
-    stat_map = {s:'mean' for s in stats}
-    correlation = pbp_data.groupby(['team', 'year']).agg(stat_map).reset_index()[stats]
+def find_correlation(X, Y):
+    #stat_map = {s:'mean' for s in stats}
+    #correlation = pbp_data.groupby(['team', 'year']).agg(stat_map).reset_index()[stats]
+    #stat_data = pbp_data.drop('result', axis=1).shift(1).dropna().reset_index()
+    correlation = pd.concat([X, Y], axis=1)
     # Calculate the correlation between the two variables
     correlation = correlation.corr()
     # Display the correlation matrix
@@ -216,13 +255,14 @@ def form_vectors(final_summary, stats):
     home = final_summary[final_summary['home_away'] == 'home']
     # get all of the home_away rows that equal away
     away = final_summary[final_summary['home_away'] == 'away']
-    vectors = home.merge(away, how='inner', on='game_id')
+    vectors = home.merge(away, how='inner', on='game_id', suffixes=('_home', '_away'))
     new_stats = []
     for stat in stats:
-        new_stats.append(stat+ '_x')
-        new_stats.append(stat+ '_y')
-    results = vectors[['game_id', 'result_x']]
-    return vectors, new_stats, results
+        new_stats.append(stat+ '_home')
+        new_stats.append(stat+ '_away')
+
+    vectors = vectors.rename(columns={'result_home': 'result'})
+    return vectors, new_stats
 
 def eval_models(X_train, X_test, y_train, y_test):
     accuracy_scores = []
@@ -255,41 +295,81 @@ def eval_models(X_train, X_test, y_train, y_test):
     print()
     print('Number of test games:', X_test.shape[0])
 
+
+def get_xy(vectors, stats):
+    X = vectors[stats]
+    y = vectors['result']
+    return X, y
+
 def main():
     parser = OptionParser()
     parser.add_option("-s", "--start_year", dest="start_year", type="int", default=2009,
                         help="The first year of data to fetch (default: 2009)")
     parser.add_option("-e", "--end_year", dest="end_year", type="int", default=2023,
                         help="The last year of data to fetch (default: 2023)")
-    parser.add_option("-w", "--end_week", dest="end_week", type="int", default=17,
+    parser.add_option("-w", "--end_week", dest="end_week", type="int", default=101,
                         help="The last week of the season to fetch (default: 17)")
     parser.add_option("-t", "--season_type", dest="season_type", default=None,
                         help="Restrict the data to a specific season type (e.g., 'REG', 'POST')")
-    parser.add_option("-m", "--model", dest="model", default="Neural Network",
+    parser.add_option("-m", "--model", dest="model", default="CatBoost",
                         help="The model to use for prediction (default: RandomForest) one of: %s" % ", ".join(models.keys()))
     parser.add_option("--eval_model", dest="eval_model", default=False, action="store_true",
                     help="Run the data through a bunch of models and spit out stats")
+    parser.add_option("--predict_year", dest="predict_year", default=None,
+                    help="Predict the outcome of games for the given year.  Both this and predict_week must be set to predict games")
+    parser.add_option("--predict_week", dest="predict_week", default=None,
+                    help="Predict the outcome of games for the given week.  Both this and predict_week must be set to predict games")
+    parser.add_option("-f","--force_data_gather", dest="force_data_gather", default=False, action="store_true",
+                    help="Force the data to be gathered again even if it is in the cache.")
     (options, args) = parser.parse_args()
 
+    if options.predict_year and options.predict_week: # if we are predicting for a given year and week, don't get any data grater than that.
+        options.end_year = int(options.predict_year)
+
     # Fetch the data with caching
-    data, stats_cols, game_cols = fetch_data_with_cache(options.start_year, options.end_year)
+    data, stats_cols, game_cols = fetch_data_with_cache(options.start_year, options.end_year, options.end_week, options)
     # only get rows where season type is REG
     data_save = data.copy()
 
-    find_correlation(data, stats_cols)
-
     data_filtered = filter_stats(data, stats_cols)
     if options.season_type:
-        data_filtered = data_filtered[data_filtered['season_type'] == 'REG']
+        data_filtered = data_filtered[data_filtered['season_type'] == options.season_type]
 
-    vectors, new_stats, output = form_vectors(data_filtered, stats_cols)
-    X = vectors[new_stats]
-    y = output['result_x']
+    vectors, new_stats = form_vectors(data_filtered, stats_cols)
+    X, Y = get_xy(vectors, new_stats)
+    find_correlation(X, Y)
 
     if options.eval_model:
         # Split the data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=False)
+        vectors = vectors.dropna()
+
+        X, Y = get_xy(vectors, new_stats)
+        # pca = PCA(n_components=9)
+        # X = pca.fit_transform(X)
+
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.4) # , shuffle=False, shuffle=False
         eval_models(X_train, X_test, y_train, y_test)
+
+    if options.predict_year and options.predict_week:
+        # get the rows that match the year and week
+        # get the prediction rows
+        predict_rows = vectors.loc[(vectors['year_home'] == int(options.predict_year)) & (vectors['week_home'] == int(options.predict_week))]
+        # get the model train rows
+        train_rows = vectors[(vectors['year_home'] < int(options.predict_year)) | ((vectors['year_home'] == int(options.predict_year)) & (vectors['week_home'] < int(options.predict_week)))]
+
+        X, Y = get_xy(train_rows, new_stats)
+        model = models[options.model]
+        model.fit(X, Y)
+        y_pred = model.predict(X)
+        print("Accuracy:", accuracy_score(Y, y_pred))
+
+        X_pred, Y_pred = get_xy(predict_rows, new_stats)
+        y_pred = model.predict(X_pred)
+        print("Predictions:")
+        for row in X_pred.iterrows():
+            print(row[1])
+            print()
+
 
 
 if __name__ == "__main__":
